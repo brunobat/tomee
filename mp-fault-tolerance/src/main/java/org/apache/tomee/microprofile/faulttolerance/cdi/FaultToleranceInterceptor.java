d65d4aeb69ab417f1700a1ba88253b5579848ee7
brunobat <brunobat@gmail.com>
2018-12-27 10:11:45 +0000

    start cdi work and place tests in project during dev time
    Signed-off-by: brunobat <brunobat@gmail.com>

--- mp-fault-tolerance/src/main/java/org/apache/tomee/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
+++ mp-fault-tolerance/src/main/java/org/apache/tomee/microprofile/faulttolerance/cdi/FaultToleranceInterceptor.java
@@ -1,5 +1,19 @@
 package org.apache.tomee.microprofile.faulttolerance.cdi;
 
+import org.eclipse.microprofile.faulttolerance.Bulkhead;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
+
+import javax.annotation.Priority;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+import java.lang.reflect.Method;
+
 /**
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,5 +30,30 @@ package org.apache.tomee.microprofile.faulttolerance.cdi;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+@Interceptor
+@FaultToleranceEnabled
+@Priority(1)
+@Dependent
 public class FaultToleranceInterceptor {
+
+    @Inject
+    private ExecutionManager executionManager;
+
+    @AroundInvoke
+    public Object runSafeguards(InvocationContext invocationContext) throws Exception {
+        if (isMethodSafeguarded(invocationContext.getMethod())) {
+            return executionManager.execute(invocationContext);
+        } else {
+            return invocationContext.proceed();
+        }
+    }
+
+    private boolean isMethodSafeguarded(Method method) {
+        return AnnotationUtil.getAnnotation(method, Retry.class) != null ||
+                AnnotationUtil.getAnnotation(method, CircuitBreaker.class) != null ||
+                AnnotationUtil.getAnnotation(method, Timeout.class) != null ||
+                AnnotationUtil.getAnnotation(method, Fallback.class) != null ||
+                AnnotationUtil.getAnnotation(method, Bulkhead.class) != null;
+    }
 }
